9fd7778266360d0949be24643a2370b3e35e2b6b
JoseDevMH <josedevmh@gmail.com>
2022-02-19 12:56:34 -0600

    Add 2 class Trabajador y PuestoLaboral

---
+++ POONomina/PuestoLaboral.cs
@@ -0,0 +1,8 @@
+namespace POONomina
+{
+    public class PuestoLaboral{
+        public string Nombre { get; set; }
+        public float  Salario { get; set; }
+        public bool EsDeBase { get; set; }
+    }
+}
---
+++ POONomina/Trabajador.cs
@@ -0,0 +1,10 @@
+namespace POONomina
+{
+    public class Trabajador{
+        public string Nombre { get; set; }
+        public string  PrimerApe { get; set; }
+        public string SegundoApe { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public char  Sexo { get; set; }
+    }
+}
