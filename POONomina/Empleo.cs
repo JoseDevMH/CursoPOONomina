21d6fc2c400b240334bb31a5bb1a00a42574d177
JoseDevMH <josedevmh@gmail.com>
2022-02-23 15:32:36 -0600

    Add Clase Empleo que tenga como propidades un trabajador, un puesto labotal, fecha de contratacion y numero de contrato

---
+++ POONomina/Empleo.cs
@@ -0,0 +1,8 @@
+namespace POONomina {
+    public class Empleo {
+        public Trabajador Trabajador { get; set; }
+        public PuestoLaboral PuestoLaboral { get; set; }
+        public DateTime FechaDeContratacion { get; set; }
+        public int NumeroDeContrato { get; set; }
+    }
+}
