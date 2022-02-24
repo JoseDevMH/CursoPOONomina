df63a92ffed6b5fa589ef459f06d06c898fb59e0
JoseDevMH <josedevmh@gmail.com>
2022-02-24 10:43:51 -0600

    Add constructor a la Clase Empleo

--- POONomina/Empleo.cs
+++ POONomina/Empleo.cs
@@ -4,5 +4,16 @@ namespace POONomina {
         public PuestoLaboral PuestoLaboral { get; set; }
         public DateTime FechaDeContratacion { get; set; }
         public int NumeroDeContrato { get; set; }
+
+        private Random r;
+
+        public Empleo(Trabajador trabajador, PuestoLaboral puesto)
+        {
+            r=new Random(DateTime.Now.Second);
+            this.Trabajador = trabajador;
+            this.PuestoLaboral = puesto;
+            this.FechaDeContratacion = DateTime.Now;
+            this.NumeroDeContrato = r.Next(2000,5000);
+        }
     }
 }
