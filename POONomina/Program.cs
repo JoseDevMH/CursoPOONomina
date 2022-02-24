04486b807c9febc4a1dcce93f6558957b59ace58
JoseDevMH <josedevmh@gmail.com>
2022-02-24 15:29:14 -0600

    Add constructor en la clase Trabajador para que pida los datos al momento de crear el trabajador

--- POONomina/Program.cs
+++ POONomina/Program.cs
@@ -3,39 +3,12 @@
 using POONomina;
 
 Trabajador t1 = new Trabajador();
-t1.Nombre = "Jose";
-t1.PrimerApe = "Garcia";
-t1.SegundoApe = "Alvarez";
-t1.FechaNacimiento = new DateTime(1981,05,26);
-t1.Sexo = 'H';
-
 Trabajador t2 = new Trabajador();
-t2.Nombre = "Carlos";
-t2.PrimerApe = "Garcia";
-t2.SegundoApe = "Roque";
-t1.FechaNacimiento = new DateTime(2000,03,11);
-t2.Sexo = 'H';
-
-Trabajador t3 = new Trabajador();
-t3.Nombre = "Julia";
-t3.PrimerApe = "Hernandez";
-t3.SegundoApe = "Silva";
-t3.FechaNacimiento = new DateTime(1995,10,17);
-t3.Sexo = 'M';
-
-Trabajador t4= new Trabajador();
-t4.Nombre = "Sofia";
-t4.PrimerApe = "Perez";
-t4.SegundoApe = "Lavou";
-t4.FechaNacimiento = new DateTime(1978,09,28);
-t4.Sexo = 'M';
-
 
 List<Trabajador> trabajadores = new List<Trabajador>();
 trabajadores.Add(t1);
 trabajadores.Add(t2);
-trabajadores.Add(t3);
-trabajadores.Add(t4);
+
 Console.WriteLine("================================================");
 Console.WriteLine($"Trabajadores en la lista: {trabajadores.Count}");
 Console.WriteLine("================================================");
@@ -46,6 +19,6 @@ foreach (Trabajador item in trabajadores)
 }
 
 Nomina nomina = new Nomina();
-Console.WriteLine("-----------------------------------------------");
+Console.WriteLine("-------------------------------------------------");
 Console.WriteLine($"Total de la nomina: {nomina.TotalDeNomina()}");
-Console.WriteLine("-----------------------------------------------");
+Console.WriteLine("-------------------------------------------------");
--- POONomina/Trabajador.cs
+++ POONomina/Trabajador.cs
@@ -7,6 +7,31 @@ namespace POONomina
         public DateTime FechaNacimiento { get; set; }
         public char  Sexo { get; set; }
 
+        public Trabajador(){
+            Console.Write("Nombre: ");
+            this.Nombre=Console.ReadLine();
+
+            Console.Write("Primer apellido: ");
+            this.PrimerApe=Console.ReadLine();
+
+            Console.Write("Segundo apellido: ");
+            this.SegundoApe=Console.ReadLine();
+
+            Console.Write("Año de nacimiento: ");
+            int anio=int.Parse(Console.ReadLine());
+
+            Console.Write("Mes de nacimiento: ");
+            int mes=int.Parse(Console.ReadLine());
+
+            Console.Write("Día de nacimiento: ");
+            int dia=int.Parse(Console.ReadLine());
+
+            this.FechaNacimiento = new DateTime(anio, mes, dia);
+
+            Console.Write("Sexo [H/M]: ");
+            this.Sexo = char.Parse(Console.ReadLine());
+        }
+
         public void MostrarDatos(){
             Console.WriteLine($"{Nombre} {PrimerApe} {SegundoApe}\t {Sexo}\t {FechaNacimiento.ToShortDateString()} ");
         }
