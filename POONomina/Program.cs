e49d0b323972c20d4e58b454066de75892f884b2
JoseDevMH <josedevmh@gmail.com>
2022-02-24 18:02:04 -0600

    Modificar el Program.cs para que permita pedir 4 trabajadores y muestre los datos

--- POONomina/Program.cs
+++ POONomina/Program.cs
@@ -2,25 +2,21 @@
 
 using POONomina;
 
-Trabajador t1 = new Trabajador("PEDRO", "JUAREZ", "HERNANDEZ", new DateTime(2018,12,12), 'H');
-Trabajador t2 = new Trabajador("MARIA", "PEREZ", "RAMIREZ", new DateTime(1999,11,13), 'M');
-Trabajador t3 = new Trabajador();
-
 List<Trabajador> trabajadores = new List<Trabajador>();
+Trabajador t1 = new Trabajador();
+Trabajador t2 = new Trabajador();
+
 trabajadores.Add(t1);
 trabajadores.Add(t2);
-trabajadores.Add(t3);
 
-Console.WriteLine("================================================");
-Console.WriteLine($"Trabajadores en la lista: {trabajadores.Count}");
-Console.WriteLine("================================================");
+for (int i = 0; i < 2; i++)
+{
+    trabajadores.Add(new Trabajador());
+}
+
+Console.WriteLine("\n\n DATOS DE LOS TRABAJADORES");
 
 foreach (Trabajador item in trabajadores)
 {
     item.MostrarDatos();
 }
-
-Nomina nomina = new Nomina();
-Console.WriteLine("-------------------------------------------------");
-Console.WriteLine($"Total de la nomina: {nomina.TotalDeNomina()}");
-Console.WriteLine("-------------------------------------------------");
