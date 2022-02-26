750c1b244c353a9a4c33a5100819ad3a2b597d5d
Jose Dev <josedevmh@gmail.com>
2022-02-26 00:28:21 -0600

    Add clase Generadora para producir unas listas de Emplados, puestos y empleos aleatorios

--- POONomina/Empleo.cs
+++ POONomina/Empleo.cs
@@ -5,11 +5,9 @@ namespace POONomina {
         public DateTime FechaDeContratacion { get; set; }
         public int NumeroDeContrato { get; set; }
 
-        private Random r;
 
-        public Empleo(Trabajador trabajador, PuestoLaboral puesto)
+        public Empleo(Trabajador trabajador, PuestoLaboral puesto, Random r)
         {
-            r=new Random(DateTime.Now.Second);
             this.Trabajador = trabajador;
             this.PuestoLaboral = puesto;
             this.FechaDeContratacion = DateTime.Now;
---
+++ POONomina/Generadora.cs
@@ -0,0 +1,52 @@
+namespace POONomina {
+    public class Generadora {
+        Random r;
+        public Generadora(int numEmpleados, int numPuestos, int numEmpleos)
+        {
+            r = new Random();
+            List<string> nombres = new List<string>();
+            nombres.Add("JOSE");
+            nombres.Add("MARIA");
+            nombres.Add("HEBER");
+            nombres.Add("MATIAS");
+            nombres.Add("EMMANUEL");
+
+            List<string> apellidos = new List<string>();
+            apellidos.Add("GARCIA");
+            apellidos.Add("ALVAREZ");
+            apellidos.Add("QUINTANILLA");
+            apellidos.Add("GRANADO");
+            apellidos.Add("ARREDONDO");
+            apellidos.Add("ACOSTA");
+            apellidos.Add("RANGEL");
+            apellidos.Add("CHIQUITO");
+            apellidos.Add("PEREZ");
+
+            List<Trabajador> trabajadores = new List<Trabajador>();
+            for (int i = 0; i < numEmpleados; i++)
+            {
+                trabajadores.Add(new Trabajador(nombres[r.Next(0, nombres.Count - 1)], apellidos[r.Next(0, apellidos.Count-1)], apellidos[r.Next(0, apellidos.Count-1)], new DateTime(r.Next(1980, 2022), r.Next(1, 12), r.Next(1, 29)), SexoAleatorio()));
+            }
+
+            List<PuestoLaboral> puestos = new List<PuestoLaboral>();
+            for (int i = 0; i < numPuestos; i++)
+            {
+                puestos.Add(new PuestoLaboral(r));
+            }
+
+            
+
+        }
+        private char SexoAleatorio()
+        {
+            if(r.NextDouble()>0.5)
+            {
+                return 'H';
+            }
+            else
+            {
+                return 'M';
+            }
+        }
+    }
+}
