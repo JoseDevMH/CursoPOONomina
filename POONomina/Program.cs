a27d7adcc3d9a3263b52c43766129bb00796b6e9
JoseDevMH <josedevmh@gmail.com>
2022-02-28 14:19:09 -0600

    Add ciclo for each para mostrar las nominas en la lista en formato de tabla

--- POONomina/Program.cs
+++ POONomina/Program.cs
@@ -11,7 +11,16 @@ for (int i = 0; i < 20; i++)
     Generadora generadora = new Generadora(20,10,15);
     nomina.Empleos = generadora.Empleos;
     DateTime FechaActual = DateTime.Now;
-    nomina.Fecha = FechaActual.AddDays(1*7);
+    nomina.Fecha = FechaActual.AddDays(i*7);
     nominas.Add(nomina);
 }
 
+Console.WriteLine("===\t ==========\t ==============");
+Console.WriteLine("NUM\t FECHA     \t TOTAL");
+Console.WriteLine("===\t ==========\t ==============");
+int j=1;
+
+foreach (Nomina item in nominas)
+{
+    Console.WriteLine($"{j++}\t {item.Fecha.ToShortDateString()}\t $ {item.TotalDeNomina()}");
+}
